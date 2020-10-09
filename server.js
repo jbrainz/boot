673136640d428c44d592d232fa36e0724b0add81
brainz <piusunimke@gmail.com>
2020-10-09 23:14:43 +0100

    express setup complete

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+//Routes Files
 
+const bootcamps = require('./routes/bootcamp');
 //Load env files
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+//Dev login middleware.
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//Mount variables
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 3000;
 

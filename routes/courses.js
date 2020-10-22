a83dc75bcb26a52aed7f9d6465a2dc13afc4d9f1
Pius <jonzyp4reala@gmail.com>
2020-10-22 08:53:28 +0100

    protected routes

--- routes/courses.js
+++ routes/courses.js
@@ -10,12 +10,18 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require('../middleware/advancedResults');
 const Course = require('../models/Course');
 
+const { protect, authorize } = require('../middleware/auth');
+
 router
   .route('/')
   .get(
     advancedResults(Course, { path: 'bootcamp', select: 'name description' }),
     getCourses
   )
-  .post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 module.exports = router;
